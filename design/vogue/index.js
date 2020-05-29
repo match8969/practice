1509b0c7ce154bd18d2fa3b4bd9b5600422d03bc
町田龍浩 <match8969@gmail.com>
2020-05-29 11:37:03 +0900

    Update

---
+++ design/vogue/index.js
@@ -0,0 +1,6 @@
+$(function(){
+    $('.header-edition').on('click', function(){    
+        $('.header-edition').toggleClass('active');
+        $('.header-edition-nav').toggleClass('active');
+    });
+});
