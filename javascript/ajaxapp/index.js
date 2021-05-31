c496c3c8ee73490077fe140da03c30de82a240dd
match8969 <33335788+match8969@users.noreply.github.com>
2021-05-31 14:39:39 +0900

    Udpate

--- javascript/ajaxapp/index.js
+++ javascript/ajaxapp/index.js
@@ -1,44 +1,30 @@
 // console.log("index.js: loaded");
 
-function main() {
-
-    // var rrr = fetchUserInfo("js-primer-example");
-    // NOTE: fetchUserInfoのreturn(response.json())　はPromise型なのに、なぜかthenでチェーンできてないっぽい。
-    //　なので、createViewまでうまくいってなそう。
-    fetchUserInfo("js-primer-example")
-        // ここではJSONオブジェクトで解決されるPromise
-        .then((userInfo) => createView(userInfo))
-        // ここではHTML文字列で解決されるPromise
-        .then((view) => displayView(view))
-        // Promiseチェーンでエラーがあった場合はキャッチされる
-        .catch((error) => {
-            console.error(`エラーが発生しました (${error})`);
-        });
+// file:///Users/match/Desktop/github/practice/javascript/ajaxapp/index.html
+async function main() {
+    try {
+        const userId = getUserId();
+        const userInfo = await fetchUserInfo(userId);
+        const view = createView(userInfo);
+        displayView(view);
+    } catch (error) {
+        console.error(`エラーが発生しました (${error})`);
+    }
 }
 
-// async function main() {
-//     try {
-//         const userInfo = await fetchUserInfo("js-primer-example");
-//         const view = createView(userInfo);
-//         displayView(view);
-//     } catch (error) {
-//         console.error(`エラーが発生しました (${error})`);
-//     }
-// }
+function getUserId(userId){
+    return document.getElementById("userId").value;
+}
 
 function fetchUserInfo(userId){
-
-    fetch(`https://api.github.com/users/${encodeURIComponent(userId)}`)
+    return fetch(`https://api.github.com/users/${encodeURIComponent(userId)}`)
     // arrow :
     .then(response =>{
         if(response.ok){
             // 200
-            console.log("200!!");
-            console.log("response.json", response.json());
             return response.json();
         }else{
             // 200以外
-            // console.log(response.status);
             return Promise.reject(new Error(`${response.status}: ${response.statusText}`));
         }
 
